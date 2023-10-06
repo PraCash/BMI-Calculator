b55684c3ad001c7f10d2fad8afa2e923583b8142
pracash <prakash_sigdel@outlook.com>
2023-10-06 17:10:49 +0300

    Updating BMICalculator.py

--- BMICalculator.py
+++ BMICalculator.py
@@ -1,20 +1,38 @@
-weight = float(input("Enter your weight in kg: "))
-height = float(input("Enter your height in cm: "))
+# Imported the necessary Flask modules.
+from flask import Flask, request, render_template
 
-height_in_meters = height / 100
+# Defiying the Flask app using 'app = Flask(--name--)'
+app = Flask(__name__)
 
-BMI = weight / height_in_meters ** 2
 
-print("Your BMI is:",  BMI)
+# Creating a route '/' that handles both GET and POST requests.
+@app.route("/", methods=["GET", "POST"])
+#Adding a 'calculate_bmi' function to calculate BMI.
+def calculate_bmi():
+    if request.method == "POST":
+        weight = float(request.form["weight"])
+        height = float(request.form["height"])
+        height_in_meters = height / 100
+        bmi = calculate_bmi_value(weight, height_in_meters)
+        category = categorize_bmi(bmi)
+        return render_template("result.html", bmi=bmi, category=category)
+    return render_template("index.html")
 
-if BMI < 18.5:
-    print ("You are Underweight")
 
-elif BMI <= 25:
-    print("You have a healthy weight ")
+def calculate_bmi_value(weight, height):
+    return round(weight / (height ** 2), 2)
 
-elif 25 <= BMI < 30:
-    print("You are OverWeight")
 
-else:
-    print("You are Obs")
+def categorize_bmi(bmi):
+    if bmi < 18.5:
+        return "Underweight"
+    elif 18.5 <= bmi < 25:
+        return "Healthy Weight"
+    elif 25 <= bmi < 30:
+        return "Overweight"
+    else:
+        return "Obese"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
